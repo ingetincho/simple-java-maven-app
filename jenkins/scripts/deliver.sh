b329bc878a4035799fd0d1926cdf4ccc185334de
Ing. Martin P. Leg <84465039+ingetincho@users.noreply.github.com>
2024-07-04 11:54:05 -0300

    0704-v04-Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -25,6 +25,6 @@ echo "Project Version = ${VERSION}"
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-dir ./target
+cd target
 java -jar ${NAME}-${VERSION}.jar
 
